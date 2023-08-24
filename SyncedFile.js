e3dfac95bb87cd0f4c3d33ddf6059596c98d432e
Emydius <emydiusmontes@gmail.com>
2023-08-23 21:23:04 -0700

    Updated README

--- SyncedFile.js
+++ SyncedFile.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 /*
-A class structure to represented files the program is currently keeping
+A class to represent files the program is currently keeping
 track of, safe enough to transfer across electron IPC channels.
 
 Figures out its own name, size, and last modified times from a path string.
